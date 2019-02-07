5dab56661822ed50a473b0b2c548825929820576
tiefengeist <sahirnambiar@gmail.com>
2019-02-07 10:45:53 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = Hash.new { |hash, key| hash[key] = {} }
+
+   data.each do |allkeys, keys|
+    keys.each do |key, values|
+      values.each do |value|
+        if result[value][allkeys]
+          result[value][allkeys] << key.to_s
+        else
+          result[value][allkeys] = [key.to_s]
+        end
+      end
+    end
+  end
+
+   result
 end
